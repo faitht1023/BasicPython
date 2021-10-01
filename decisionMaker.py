64194483b43575e90a2c1c16f9ec126e65fc2510
faitht1023 <43914867+faitht1023@users.noreply.github.com>
2021-10-01 14:48:39 -0700

    Create decisionMaker.py

---
+++ decisionMaker.py
@@ -0,0 +1,10 @@
+# Created Oct 1, 2021
+# Creator: Faith
+
+# importing the random module
+import random
+
+answers = ["definitely yes", "don't do it", "go for it", "maybe don't do that", "yesyesyes", "not a great idea..."]
+
+#return a random answer
+print(answers[random.randint(0,5)])
